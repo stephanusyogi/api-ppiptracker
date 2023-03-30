192ab4a410884c9a8743e7f898a2c123f84e4e96
stephanus yogi <stephanusyogi12@gmail.com>
2023-03-30 08:20:38 +0700

    progress

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
         }
 
         // $users = $query->latest()->paginate($per_page ? $per_page : 2);
-        $users = $query->all();
+        $users = User::all();
 
         return new UserResource(true, 'List Data Users!', $users);
     }
