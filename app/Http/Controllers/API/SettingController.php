318a4947863ca4af2f27d72798d9ef210206b385
stephanus yogi <stephanusyogi12@gmail.com>
2023-04-17 14:14:18 +0700

    progress

--- app/Http/Controllers/API/SettingController.php
+++ app/Http/Controllers/API/SettingController.php
@@ -24,20 +24,94 @@ class SettingController extends Controller
         "data" => $setting_nilai_asumsi
     ],200);
   }
-  public function setting_nilai_asumsi_update(Request $request){
-  $affected = DB::table('nilai_asumsi_admin')
-    ->where('id', $request->id)
-    ->update([
-      'kenaikan_gaji' => $request->kenaikan_gaji,
-      'kenaikan_phdp' => $request->kenaikan_phdp,
-      'iuran_ppip' => $request->iuran_ppip,
-      'dasar_pembayaran_iuran_personal' => $request->dasar_pembayaran_iuran_personal,
-      'inflasi_jangka_panjang' => $request->inflasi_jangka_panjang,
+  public function setting_nilai_asumsi_update(Request $request)
+  {
+    $affected = DB::table('nilai_asumsi_admin')
+      ->where('id', $request->id)
+      ->update([
+        'kenaikan_gaji' => $request->kenaikan_gaji,
+        'kenaikan_phdp' => $request->kenaikan_phdp,
+        'iuran_ppip' => $request->iuran_ppip,
+        'dasar_pembayaran_iuran_personal' => $request->dasar_pembayaran_iuran_personal,
+        'inflasi_jangka_panjang' => $request->inflasi_jangka_panjang,
+      ]);
+    return response()->json([
+        "status" =>true,
+        "message"=>"Setting Nilai Asumsi Updated!",
+    ],200);
+  }
+
+  public function setting_ppip(){
+    $setting_ppip = DB::table('setting_portofolio_ppip_admin')
+    ->select('*')->get();
+
+    $opsi = DB::table('setting_portofolio_ppip_admin')
+    ->select('nama_portofolio')->get();
+
+    return response()->json([
+        "status" =>true,
+        "message"=>"Lists Setting PPIP!",
+        "opsi" => $opsi,
+        "data" => $setting_ppip
+    ],200);
+  }
+  public function setting_ppip_add(Request $request){
+    DB::table('setting_portofolio_ppip_admin')->insert([
+        'id' => (string) Str::uuid(),
+        'nama_portofolio' => $request->nama_portofolio,
+        'return_saham' => $request->return_saham,
+        'return_pendapatan_tetap' => $request->return_pendapatan_tetap,
+        'return_deposito' => $request->return_deposito,
+        'resiko_saham' => $request->resiko_saham,
+        'resiko_pendapatan_tetap' => $request->resiko_pendapatan_tetap,
+        'resiko_deposito' => $request->resiko_deposito,
+        'korelasi_saham_pendapatan_tetap' => $request->korelasi_saham_pendapatan_tetap,
+        'korelasi_saham_deposito' => $request->korelasi_saham_deposito,
+        'korelasi_pendapatan_tetap_deposito	' => $request->korelasi_pendapatan_tetap_deposito,
+        'tranche_investasi_saham	' => $request->tranche_investasi_saham,
+        'tranche_investasi_pendapatan_tetap		' => $request->tranche_investasi_pendapatan_tetap	,
+        'tranche_investasi_deposito	' => $request->tranche_investasi_deposito,
+        'tranche_likuiditas_saham	' => $request->tranche_likuiditas_saham,
+        'tranche_likuiditas_pendapatan_tetap		' => $request->tranche_likuiditas_pendapatan_tetap,
+        'tranche_likuiditas_deposito		' => $request->tranche_likuiditas_deposito,
+        'return_portofolio_tranche_investasi		' => $request->return_portofolio_tranche_investasi,
+        'return_portofolio_tranche_likuiditas		' => $request->return_portofolio_tranche_likuiditas,
+        'flag		' => 1,
     ]);
-  return response()->json([
+    
+    return response()->json([
       "status" =>true,
-      "message"=>"Setting Nilai Asumsi Updated!",
-      "data" => $affected
-  ],200);
+      "message"=>"Setting PPIP Berhasil Ditambahkan!",
+    ],200);
+  }
+  public function setting_ppip_update(Request $request)
+  {
+    $affected = DB::table('setting_portofolio_ppip_admin')
+      ->where('id', $request->id)
+      ->update([
+        'nama_portofolio' => $request->nama_portofolio,
+        'return_saham' => $request->return_saham,
+        'return_pendapatan_tetap' => $request->return_pendapatan_tetap,
+        'return_deposito' => $request->return_deposito,
+        'resiko_saham' => $request->resiko_saham,
+        'resiko_pendapatan_tetap' => $request->resiko_pendapatan_tetap,
+        'resiko_deposito' => $request->resiko_deposito,
+        'korelasi_saham_pendapatan_tetap' => $request->korelasi_saham_pendapatan_tetap,
+        'korelasi_saham_deposito' => $request->korelasi_saham_deposito,
+        'korelasi_pendapatan_tetap_deposito	' => $request->korelasi_pendapatan_tetap_deposito,
+        'tranche_investasi_saham	' => $request->tranche_investasi_saham,
+        'tranche_investasi_pendapatan_tetap		' => $request->tranche_investasi_pendapatan_tetap	,
+        'tranche_investasi_deposito	' => $request->tranche_investasi_deposito,
+        'tranche_likuiditas_saham	' => $request->tranche_likuiditas_saham,
+        'tranche_likuiditas_pendapatan_tetap		' => $request->tranche_likuiditas_pendapatan_tetap,
+        'tranche_likuiditas_deposito		' => $request->tranche_likuiditas_deposito,
+        'return_portofolio_tranche_investasi		' => $request->return_portofolio_tranche_investasi,
+        'return_portofolio_tranche_likuiditas		' => $request->return_portofolio_tranche_likuiditas,
+        'flag		' => $request->flag,
+      ]);
+    return response()->json([
+        "status" =>true,
+        "message"=>"Setting Nilai Asumsi Diperbarui!",
+    ],200);
   }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -15,6 +15,10 @@ Route::group(['middleware' => ['auth:admin,api-admin']], function(){
 
     Route::get('/setting-nilai-asumsi/admin', [SettingController::class, 'setting_nilai_asumsi']);
     Route::post('/setting-nilai-asumsi/admin/update', [SettingController::class, 'setting_nilai_asumsi_update']);
+    
+    Route::get('/setting-ppip/admin', [SettingController::class, 'setting_ppip']);
+    Route::post('/setting-ppip/admin/add', [SettingController::class, 'setting_ppip_add']);
+    Route::post('/setting-ppip/admin/update', [SettingController::class, 'setting_ppip_update']);
 
     Route::get('/logout-administrator', [AdminController::class, 'logout']);
     Route::get('/check-token', [AdminController::class, 'checktoken']);
