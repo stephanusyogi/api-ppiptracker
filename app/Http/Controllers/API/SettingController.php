12e225c55089c3e57c6c4c8182d858da84d54fa7
stephanus yogi <stephanusyogi12@gmail.com>
2023-04-19 09:33:15 +0700

    progress

--- app/Http/Controllers/API/SettingController.php
+++ app/Http/Controllers/API/SettingController.php
@@ -142,10 +142,17 @@ class SettingController extends Controller
           "data" => $setting_ppip
       ],200);
     } else {
-      $setting_ppip = DB::table('setting_portofolio_personal_admin')->select('*')
-      ->leftjoin('setting_komposisi_investasi_lifecycle_fund_admin', 'setting_portofolio_personal_admin.id', '=', 'setting_komposisi_investasi_lifecycle_fund_admin.id_setting_portofolio_personal_admin')
-      ->groupBy('setting_portofolio_personal_admin.id')
-      ->get();
+      $setting_ppip = DB::table('setting_portofolio_personal_admin')
+        ->select('*')
+        ->selectRaw('(SELECT * FROM setting_komposisi_investasi_lifecycle_fund_admin WHERE setting_portofolio_personal_admin.id = setting_komposisi_investasi_lifecycle_fund_admin.id_setting_portofolio_personal_admin) AS komposisi_investasi')
+        ->groupBy('setting_portofolio_personal_admin.id')
+        ->where('setting_portofolio_personal_admin.id', $id)
+        ->get();
+
+      // $setting_ppip = DB::table('setting_portofolio_personal_admin')->select('*')
+      // ->leftjoin('setting_komposisi_investasi_lifecycle_fund_admin', 'setting_portofolio_personal_admin.id', '=', 'setting_komposisi_investasi_lifecycle_fund_admin.id_setting_portofolio_personal_admin')
+      // ->groupBy('setting_portofolio_personal_admin.id')
+      // ->get();
   
         return response()->json([
             "status" =>true,
@@ -155,4 +162,31 @@ class SettingController extends Controller
         ],200);
     }
   }
+  // public function setting_personal_lifecycle_add(Request $request){
+  //   DB::table('setting_portofolio_ppip_admin')->insert([
+  //       'id' => (string) Str::uuid(),
+  //       'nama_portofolio' => $request->nama_portofolio,
+  //       'return_saham' => $request->return_saham,
+  //       'return_pendapatan_tetap' => $request->return_pendapatan_tetap,
+  //       'return_deposito' => $request->return_deposito,
+  //       'resiko_saham' => $request->resiko_saham,
+  //       'resiko_pendapatan_tetap' => $request->resiko_pendapatan_tetap,
+  //       'resiko_deposito' => $request->resiko_deposito,
+  //       'korelasi_saham_pendapatan_tetap' => $request->korelasi_saham_pendapatan_tetap,
+  //       'korelasi_saham_deposito' => $request->korelasi_saham_deposito,
+  //       'korelasi_pendapatan_tetap_deposito' => $request->korelasi_pendapatan_tetap_deposito,
+  //       'tranche_investasi_saham' => $request->tranche_investasi_saham,
+  //       'tranche_investasi_pendapatan_tetap' => $request->tranche_investasi_pendapatan_tetap,
+  //       'tranche_investasi_deposito' => $request->tranche_investasi_deposito,
+  //       'tranche_likuiditas_saham' => $request->tranche_likuiditas_saham,
+  //       'tranche_likuiditas_pendapatan_tetap' => $request->tranche_likuiditas_pendapatan_tetap,
+  //       'tranche_likuiditas_deposito' => $request->tranche_likuiditas_deposito,
+  //       'flag' => 1,
+  //   ]);
+    
+  //   return response()->json([
+  //     "status" =>true,
+  //     "message"=>"Setting PPIP Berhasil Ditambahkan!",
+  //   ],200);
+  // }
 }
