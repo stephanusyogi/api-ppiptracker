24fb95b97539b9adfccfdbd95b3df21d897a4d99
stephanus yogi <stephanusyogi12@gmail.com>
2023-04-21 17:56:12 +0700

    Progress

--- app/Http/Controllers/API/SettingController.php
+++ app/Http/Controllers/API/SettingController.php
@@ -284,6 +284,7 @@ class SettingController extends Controller
     ->get()->toArray();
     // $array = get_object_vars($response);
     var_dump($response);
+    var_dump($request->nama);
     die();
 
     DB::table('setting_komposisi_investasi_lifecycle_fund_admin')->insert([
