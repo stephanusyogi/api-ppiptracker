7fc6f62e5cc599312b4222acb3977b9112b77bca
stephanus yogi <stephanusyogi12@gmail.com>
2023-04-24 22:08:27 +0700

    Progress

--- app/Http/Controllers/API/SettingController.php
+++ app/Http/Controllers/API/SettingController.php
@@ -153,8 +153,6 @@ class SettingController extends Controller
       $return_ppip_likuid = round(($komposisi_likuid_saham_ppip*$return_saham_ppip + $komposisi_likuid_fi_ppip*$return_fi_ppip + $komposisi_likuid_depo_ppip*$return_depo_ppip) / 100, 2); 
 
       // perhitungan resiko portofolio tranche investasi
-      // $risk_ppip_inv_step1 = $komposisi_inv_saham_ppip*$komposisi_inv_saham_ppip*$risk_saham_ppip*$risk_saham_ppip + $komposisi_inv_fi_ppip*$komposisi_inv_fi_ppip*$risk_fi_ppip*$risk_fi_ppip + $komposisi_inv_depo_ppip*$komposisi_inv_depo_ppip*$risk_depo_ppip*$risk_depo_ppip;
-      // $risk_ppip_inv_step2 = 2*$komposisi_inv_saham_ppip*$komposisi_inv_fi_ppip*$risk_saham_ppip*$risk_fi_ppip*$korel_saham_fi_ppip+ 2*$komposisi_inv_saham_ppip*$komposisi_inv_depo_ppip*$risk_saham_ppip*$risk_depo_ppip*$korel_saham_depo_ppip+2*$komposisi_inv_fi_ppip*$komposisi_inv_depo_ppip*$risk_fi_ppip*$risk_depo_ppip*$korel_fi_depo_ppip;
       $risk_ppip_inv_step1 = $komposisi_inv_saham_ppip * $komposisi_inv_saham_ppip * $risk_saham_ppip * $risk_saham_ppip + $komposisi_inv_fi_ppip * $komposisi_inv_fi_ppip * $risk_fi_ppip * $risk_fi_ppip + $komposisi_inv_depo_ppip * $komposisi_inv_depo_ppip * $risk_depo_ppip * $risk_depo_ppip;
       $risk_ppip_inv_step2 = 2 * $komposisi_inv_saham_ppip * $komposisi_inv_fi_ppip * $risk_saham_ppip * $risk_fi_ppip * $korel_saham_fi_ppip + 2 * $komposisi_inv_saham_ppip * $komposisi_inv_depo_ppip * $risk_saham_ppip * $risk_depo_ppip * $korel_saham_depo_ppip + 2 * $komposisi_inv_fi_ppip * $komposisi_inv_depo_ppip * $risk_fi_ppip * $risk_depo_ppip * $korel_fi_depo_ppip;
       $risk_ppip_inv = round(sqrt($risk_ppip_inv_step1+$risk_ppip_inv_step2) / 100, 2);
@@ -163,11 +161,22 @@ class SettingController extends Controller
       $risk_ppip_likuid_step1 = $komposisi_likuid_saham_ppip*$komposisi_likuid_saham_ppip*$risk_saham_ppip*$risk_saham_ppip + $komposisi_likuid_fi_ppip*$komposisi_likuid_fi_ppip*$risk_fi_ppip*$risk_fi_ppip + $komposisi_likuid_depo_ppip*$komposisi_likuid_depo_ppip*$risk_depo_ppip*$risk_depo_ppip;
       $risk_ppip_likuid_step2 = 2*$komposisi_likuid_saham_ppip*$komposisi_likuid_fi_ppip*$risk_saham_ppip*$risk_fi_ppip*$korel_saham_fi_ppip+ 2*$komposisi_likuid_saham_ppip*$komposisi_likuid_depo_ppip*$risk_saham_ppip*$risk_depo_ppip*$korel_saham_depo_ppip+2*$komposisi_likuid_fi_ppip*$komposisi_likuid_depo_ppip*$risk_fi_ppip*$risk_depo_ppip*$korel_fi_depo_ppip;
       $risk_ppip_likuid = round(sqrt($risk_ppip_likuid_step1+$risk_ppip_likuid_step2) / 100, 2);
-      echo "resiko investasi: ".$risk_ppip_inv;
-      echo "<br/>";
-      echo "resiko investasi: ".$risk_ppip_likuid;
-      echo "<br/>";
-      echo "-----";
+
+      // Update Tabel
+      DB::table('setting_portofolio_ppip_admin')
+        ->where('id', $obj->id)
+        ->update([
+          'return_portofolio_tranche_investasi' => $return_ppip_inv,
+          'return_portofolio_tranche_likuiditas' => $return_ppip_likuid,
+          'resiko_portofolio_tranche_investasi' => $risk_ppip_inv,
+          'resiko_portofolio_tranche_likuiditas' => $risk_ppip_likuid,
+          'flag' => 1,
+        ]);
+
+        return response()->json([
+          "status" =>true,
+          "message"=>"Setting Updated!",
+        ],200); 
     }
   }
   
