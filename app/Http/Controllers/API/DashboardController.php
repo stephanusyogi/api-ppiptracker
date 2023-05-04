897b35868164441ab0db058a25b48a364da0ce88
stephanus yogi <stephanusyogi12@gmail.com>
2023-05-04 14:28:31 +0700

    Progress

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -24,16 +24,25 @@ class DashboardController extends Controller
         ->get()[0];
       $target_replacement_ratio = round($res->answer,2);
 
+      // -----------------------------------------------------------------------
+
       //B.1 Hitung usia diangkat
       $res = User::select('tgl_lahir','tgl_diangkat_pegawai')->where('id',$id_user)->get()[0];
       $date1 = date_create($res->tgl_lahir); //Read tanggal lahir
       $date2 = date_create($res->tgl_diangkat_pegawai); //Read tanggal diangkat
+
       $diff = date_diff($date1,$date2);
+
+      $tahun = $diff->format('%y');
+      $bulan = $diff->format('%m');
       
       return response()->json([
         "status" =>true,
         "message"=>"Testing Hitung Awal!",
-        "data_testing" => $diff
+        "data_testing" => array(
+          "tahun" => $tahun,
+          "bulan" => $bulan,
+        )
       ],200);
     }
 }
