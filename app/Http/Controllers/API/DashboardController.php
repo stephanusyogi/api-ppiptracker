fc54d7bdfc61850046be2fef0564c302f3f10ae4
stephanus yogi <stephanusyogi12@gmail.com>
2023-05-06 13:11:57 +0700

    Progress

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -188,7 +188,7 @@ class DashboardController extends Controller
     $tabel_norminv = DB::table('distribusi_normal')->select('norm_inv')
       ->get()->toArray();
     for ($i=1;$i<count($tabel_norminv);$i++){ //$i adalah primary key dari tabel normal inverse yang ada di database
-        $norminv[$i]=$tabel_norminv[$i];//Read tabel normal inverse
+        $norminv[$i]=$tabel_norminv[$i]['norm_inv'];//Read tabel normal inverse
     }
     for($year=2023; $year<=2100; $year++){
 
