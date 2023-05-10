f82f56477109f3e6aa5dd611905a72877eea6a4e
Stephanus Pradipta Yogi Setiawan <60417127+stephanusyogi@users.noreply.github.com>
2023-05-10 16:06:23 +0700

    Update DashboardController.php

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -68,7 +68,7 @@ class DashboardController extends Controller
               $usia_bulan[$key_bulan] = $bulan;
           }
       }
-        echo json_encode($usia_tahun, true);
+        echo json_encode($usia_bulan, true);
       die();
       // -----------------------------------------------------------------------
       //C.2. Simulasi Basic - hitung Masa Dinas (masa dinas diisi dari januari 2023 s.d. desember 2100)
