8701f2622e7edb7135016729187c9e59a0d6fdb1
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-05-12 15:04:09 +0700

    Update DashboardController.php

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -46,11 +46,13 @@ class DashboardController extends Controller
       $date2=date_create("2023-01-31"); //januari 2023
       $diff=date_diff($date1,$date2);
       
+      /* cek tahun dan bulan
       $tahun=(int)$diff->format('%y');
       $bulan=(int)$diff->format('%m');
       echo json_encode($tahun, true);
       echo json_encode($bulan, true);
       die();
+      */
 
       //Output: Create $tahun dan $bulan ke masing-masing tahun dan bulan di database usia 
       $usia_tahun = array();
@@ -76,8 +78,8 @@ class DashboardController extends Controller
               $usia_bulan[$key_bulan] = $bulan;
           }
       }
-       // echo json_encode($usia_tahun, true);
-      //  die();
+       echo json_encode($usia_tahun, true);
+       die();
       // -----------------------------------------------------------------------
       //C.2. Simulasi Basic - hitung Masa Dinas (masa dinas diisi dari januari 2023 s.d. desember 2100)
       $date1=date_create($data_user->tgl_diangkat_pegawai); //Read tanggal diangkat
