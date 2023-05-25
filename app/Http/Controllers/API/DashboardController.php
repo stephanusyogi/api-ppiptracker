d7a6e8b7184b0482f3dbab27d6f21790fb20d730
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-05-25 13:37:27 +0700

    Update DashboardController.php

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -132,7 +132,7 @@ class DashboardController extends Controller
        
        echo json_encode($masa_dinas_bulan, true);
        echo json_encode($masa_dinas_tahun, true);
-       //die();
+       die();
        
        // -----------------------------------------------------------------------
        //C.3. Simulasi Basic - sisa masa kerja (sisa masa kerja diisi dari januari 2023 s.d. desember 2100)
