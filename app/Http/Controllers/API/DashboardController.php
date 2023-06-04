cfc7b1a8a87c7a85f682f7b06f4333cd2d8b0398
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-06-05 04:33:22 +0700

    Update DashboardController.php

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -1845,8 +1845,8 @@ class DashboardController extends Controller
       $dashboard_penghasilan_bulanan_ppip_med_pv = $dashboard_penghasilan_bulanan_ppip_med / ((1+$inflasi)^($tahun_sisa_kerja+($bulan_sisa_kerja/12)));
       $dashboard_penghasilan_bulanan_ppip_max_pv = $dashboard_penghasilan_bulanan_ppip_max / ((1+$inflasi)^($tahun_sisa_kerja+($bulan_sisa_kerja/12)));
       
-      echo json_encode($dashboard_penghasilan_bulanan_ppip_min, true);
-      //echo json_encode($dashboard_penghasilan_bulanan_ppip_min_pv, true);
+      //echo json_encode($dashboard_penghasilan_bulanan_ppip_min, true);
+      echo json_encode($dashboard_penghasilan_bulanan_ppip_min_pv, true);
       die();
 
       $dashboard_penghasilan_bulanan_personal_keuangan_min_pv = $dashboard_penghasilan_bulanan_personal_keuangan_min / ((1+$inflasi)^($tahun_sisa_kerja+($bulan_sisa_kerja/12)));
