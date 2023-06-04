f068608c54c43be87087a0809035627037e38527
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-06-05 05:11:56 +0700

    Update DashboardController.php

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -335,8 +335,15 @@ class DashboardController extends Controller
        //----------------------------------------------------------------------------
        //G.1. Hitung indikator dashboard - lokasi pensiun4
        $return_dashboard = $this->indikator_dashboard($data_user, $id_user, $flag_pensiun, $sisa_kerja_tahun, $sisa_kerja_bulan, $return_simulasi_ppip, $return_simulasi_personal_properti, $return_simulasi_personal_keuangan, $return_simulasi_ppmp);
-       echo json_encode($return_dashboard, true);
+       //echo json_encode($return_dashboard, true);
+       //die();
+       
+       //--------------------------------------------------------
+       //H.1. dan H.2. Hitung selisih target dan kekurangan iuran personal keuangan
+        //input 
+        echo json_encode($target_replacement_ratio, true);
        die();
+        //$target_replacement_ratio
         
        return response()->json([
          "status" =>true,
