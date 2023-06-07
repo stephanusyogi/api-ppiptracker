bb7c9ff1ad0c0f1d205ee422b9490f0f1c26620e
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-06-07 07:41:35 +0700

    Update DashboardController.php

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -104,7 +104,7 @@ class DashboardController extends Controller
       $gaji = $request->gaji;
       $phdp = $request->phdp;
       
-      echo json_encode($request, true);
+      echo json_encode($gaji, true);
       die();
 
       //A.1 Hitung Target Replacement Ratio
