f83194249e37fa18285588f4b52ccc1f1422c47a
Stephanus Pradipta Yogi Setiawan <60417127+stephanusyogi@users.noreply.github.com>
2023-06-13 17:26:01 +0700

    Update DashboardController.php

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -411,6 +411,8 @@ class DashboardController extends Controller
       $return_dashboard["rekomendasi"] = $rekomendasi;
         
       $this->uploadToDatabase("dashboard", $id_user, $return_dashboard);
+        echo json_encode($return_dashboard, true);
+        die();
         
       return response()->json([
         "status" =>true,
